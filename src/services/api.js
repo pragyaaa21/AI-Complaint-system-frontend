21322a24c6bda8ae7fd4277100d553d72c63a12e
pragya <pragya21ch@gmail.com>
2026-05-19 11:15:07 +0530

    Connected frontend to backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+ baseURL: "https://ai-complaint-system-9frv.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
